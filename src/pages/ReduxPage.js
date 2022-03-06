28146dc0b9dc919b6314d61d0ebf3396a9bc85c3
bubucuo <276185876@qq.com>
2022-03-06 19:31:08 +0800

    init

---
+++ src/pages/ReduxPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import store from "../store/";
+
+export default class ReduxPage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>ReduxPage</h3>
+      </div>
+    );
+  }
+}
